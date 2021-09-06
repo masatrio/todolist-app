e16953f31b7ad86a83bed9de6a467ee1a6d6f8e8
J.T.A.S <jeems.terry@gmail.com>
2021-09-07 01:31:16 +0700

    generate tb_status migration & model

---
+++ app/db/models/tb_status.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class tb_status extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+
+    toJSON(){
+      return  {
+        ...this.get(), createdAt: undefined, updatedAt: undefined
+      };
+    }
+  };
+  tb_status.init({
+    name: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'tb_status',
+  });
+  return tb_status;
+};
