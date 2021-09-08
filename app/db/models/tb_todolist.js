f6bdb0175c8f672a6027c6cce99c29a9eeef7313
FellowMustard <rachmawanng33@gmail.com>
2021-09-08 14:36:27 +0700

    Adding tb_todolist repository & Model

---
+++ app/db/models/tb_todolist.js
@@ -0,0 +1,33 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class tb_todolist extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.belongsTo(tb_todo, { foreignKey: 'id_todo' }),
+      this.belongsTo(tb_status, { foreignKey: 'id_status' });
+      // define association here
+    }
+  };
+  tb_todolist.init({
+    id_todolist:{
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    id_todo: DataTypes.STRING,
+    task: DataTypes.STRING,
+    id_status: DataTypes.STRING,
+    created_at: DataTypes.DATE,
+    updated_at: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'tb_todolist',
+  });
+  return tb_todolist;
+};
